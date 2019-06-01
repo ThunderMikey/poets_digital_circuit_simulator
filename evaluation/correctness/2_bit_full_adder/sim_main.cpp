7fb24e9766cb0f35e87705e52e64d6accb863517
Mike Chen <mike@tmikey.tech>
2019-06-01 16:52:59 +0100

    fix fulladder sim logic

--- evaluation/correctness/2_bit_full_adder/sim_main.cpp
+++ evaluation/correctness/2_bit_full_adder/sim_main.cpp
@@ -7,12 +7,14 @@ int main (int argc, char** argv, char** env){
   Vfulladder* top = new Vfulladder;
   for(int i=0; i<8; i++){
     top->x = i & 1;
-    top->y = i & 2;
-    top->cin=i & 4;
+    top->y = (i & 2)>>1;
+    top->cin=(i & 4)>>2;
     top->eval();
-    std::cout<<"input: "<<int(top->x)
-      <<", A: "<<int(top->A)
-      <<", cout: "<<int(top->cout)<<"\n";
+    unsigned output = 0;
+    output |= top->A & 1;
+    output |= (top->cout &1)<<1;
+    std::cout<<"input: "<<unsigned(i)
+      <<", output: "<<unsigned(output)<<"\n";
   }
   top->final();
   delete top;
