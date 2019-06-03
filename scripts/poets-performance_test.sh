5eb205057a83f2ff2970eb05ccf81ea892763d4b
Mike Chen <mike@tmikey.tech>
2019-06-03 21:18:53 +0100

    change log name

--- scripts/poets-performance_test.sh
+++ scripts/poets-performance_test.sh
@@ -5,7 +5,6 @@
 # $1: POETS XML file directory
 #   needs to have individual XML file in each subsir
 
-PTS_LOG=pts.log
 PTS_T_LOG=/tmp/pts_run_time.log
 TIME_LOG=`pwd`/time_log.csv
 PTS_XMLS=`find $1 -type f -name *.xml`
@@ -16,7 +15,6 @@ source /local/ecad/setup-quartus17v0.bash
 # delete previous logs
 for xml in ${PTS_XMLS}; do
   pushd `dirname $xml` > /dev/null
-  rm $PTS_LOG
   # generate tinsel.elf
   pts-xmlc `basename $xml` > /dev/null
   popd > /dev/null
@@ -29,9 +27,10 @@ for xml in ${PTS_XMLS}; do
   # random int between 1 and 10
   sleep_t=$(shuf -i 1-10 -n 1)
   sleep $sleep_t
-  echo -n "${xml%.*}, $sleep_t, " >> $TIME_LOG
+  app_name=`basename ${xml} .xml`
+  echo -n "${app_name}, $sleep_t, " >> $TIME_LOG
   pushd `dirname $xml` > /dev/null
-  /usr/bin/time -f "%e, %U, %S" -o $PTS_T_LOG pts-serve --headless 1 >> $PTS_LOG 2>&1
+  /usr/bin/time -f "%e, %U, %S" -o $PTS_T_LOG pts-serve --headless 1 > ${app_name}.log 2>&1
   # append times
   awk -F "," '/appWallClockTime/{printf $2", "}' measure.csv >> $TIME_LOG
   popd > /dev/null
