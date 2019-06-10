291c1e04982782ae34603a0ee0440d5079395e05
Mike Chen <mike@tmikey.tech>
2019-06-10 11:56:14 +0100

    restructor Verilator testbench

---
+++ evaluation/corr-verilator/generate_test_instances.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TEMPLATE=./template/template.v
+INST_DIR=instances
+BEGIN_WIDTH=$1
+END_WIDTH=$2
+MAX_DEPTH=$3
+STEP_DEPTH=$4
+
+DRIVER=sim_main.cpp
+
+mkdir -p $INST_DIR
+
+for ((d=1; d<=${MAX_DEPTH}; d+=$STEP_DEPTH)); do
+  for ((w=$BEGIN_WIDTH; w<=${END_WIDTH}; w++)); do
+    inst=${w}x${d}
+    inst_path=$INST_DIR/$inst
+    inst_v=${inst_path}/${inst}.v
+    cp -rT template $inst_path
+    mv ${inst_path}/template.v ${inst_v}
+    sed -i "s/T_IO_PAIRS/${w}/g" ${inst_v}
+    sed -i "s/T_DEPTH/${d}/g" ${inst_v}
+    sed -i "s/T_PROG/${inst}/g" $inst_path/${DRIVER}
+    sed -i "s/T_ITERATION/$((2 ** ($w*2)))/g" $inst_path/${DRIVER}
+    sed -i "s/T_PROG/${inst}/g" $inst_path/make_run.sh
+  done
+done
--- evaluation/corr-verilator/run_all.sh
+++ evaluation/corr-verilator/run_all.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # run `make_run.sh` in each directory
 
-tests=`ls -d */`
+TEST_DIR=$1
+
+#tests=`ls -d */`
+tests=`ls $TEST_DIR`
 
 for t in ${tests}; do
-  pushd $t >/dev/null
+  pushd $TEST_DIR/$t >/dev/null
   ./make_run.sh
   popd >/dev/null
 done
